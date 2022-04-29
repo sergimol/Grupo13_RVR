5fb3fd91960bef7885609b86812a59eb3f03bd09
sergimol <sergimol@ucm.es>
2022-04-29 11:47:11 +0200

    Ejercicio 2.3 ahora si
    Co-authored-by:  AndresCarnicero<acarni01@ucm.es>

--- practica2.2/serializacion/ejercicio1.cc
+++ practica2.2/serializacion/ejercicio1.cc
@@ -21,26 +21,27 @@ public:
 
     void to_bin()
     {
-        // Reserva memoria para todos los datos
-        alloc_data(sizeof(char) * MAX_NAME + sizeof(int16_t) * 2);
-        char* buffer = _data;
-        // Copìa el nombre en el buffer
-        memcpy(buffer, (void*)name, sizeof(char) * MAX_NAME);
-        buffer += sizeof(char) * MAX_NAME;
-        // Copìa la posición de x en el buffer
-        memcpy(buffer, &x, sizeof(int16_t));
-        buffer += sizeof(int16_t);
-        // Copìa la posición de x en el buffer
-        memcpy(buffer, &y, sizeof(int16_t));
+        // Asigna memoria para los datos
+        int32_t jugador_size = 2 * sizeof(int16_t) + MAX_NAME * sizeof(char);
+        alloc_data(jugador_size);
+        char * tmp = _data;
+        // Copia el nombre
+        memcpy(tmp, name, MAX_NAME * sizeof(char));
+        tmp += MAX_NAME * sizeof(char);
+        // Copia la x
+        memcpy(tmp, &x, sizeof(int16_t));
+        tmp += sizeof(int16_t);
+        // Copia la y
+        memcpy(tmp, &y, sizeof(int16_t));
     }
 
     int from_bin(char * data)
     {
         // Copia data en el buffer
-        char* buffer = data + sizeof(int32_t);
+        char* buffer = data;
         // Copia el nombre del buffer
-        memcpy(name, buffer, MAX_NAME);
-        buffer += MAX_NAME;
+        memcpy(name, buffer, MAX_NAME * sizeof(char));
+        buffer += MAX_NAME * sizeof(char);
         // Copia x del buffer
         memcpy(&x, buffer, sizeof(int16_t));
         buffer += sizeof(int16_t);
@@ -71,11 +72,11 @@ int main(int argc, char **argv)
     close(file);
     // 3. Leer el fichero
     char buffer[256];
-    file = ("./jugador.bin", O_RDONLY);
+    file = open("./jugador.bin", O_RDONLY);
     read(file, buffer, 256);
-    close(file);
     // 4. "Deserializar" en one_r
     one_r.from_bin(buffer);
+    close(file);
     // 5. Mostrar el contenido de one_r
     std::cout << "Name: " << one_r.name << " X: " << one_r.x << " Y: " << one_r.y << "\n";
 
